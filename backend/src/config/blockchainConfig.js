ee362ddb2e20783fd1a0577a801f7c10726e6355
emmanuelist <emmanuel.paul75@yahoo.com>
2025-02-08 02:22:15 +0100

    Add blockchain configuration for Stellar and Ethereum
    - Define Stellar network and server URL for TESTNET
    - Define Ethereum provider URL for Rinkeby using Infura

---
+++ backend/src/config/blockchainConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+	stellar: {
+		network: 'TESTNET',
+		server: 'https://horizon-testnet.stellar.org',
+	},
+	ethereum: {
+		provider: 'https://rinkeby.infura.io/v3/YOUR_INFURA_PROJECT_ID',
+	},
+};
