ddf5d1fb4fd4d2b86fcb52085f2ab434a07c9283
emmanuelist <emmanuel.paul75@yahoo.com>
2025-02-09 01:17:59 +0100

    Refactor calculateFee function to dynamically fetch gas prices for Ethereum and fee stats for Stellar

--- backend/src/utils/feeCalculator.js
+++ backend/src/utils/feeCalculator.js
@@ -1,7 +1,16 @@
-exports.calculateFee = (chain, amount) => {
-	const fees = {
-		stellar: 0.01,   // XLM fee per tx
-		ethereum: 0.001  // ETH fee approx
-	};
-	return fees[chain];
-};
+const Web3 = require('web3');
+const StellarSdk = require('stellar-sdk');
+
+async function calculateFee(chain) {
+	if (chain === 'ethereum') {
+		const web3 = new Web3('https://sepolia.infura.io/v3/YOUR_INFURA_PROJECT_ID');
+		const gasPrice = await web3.eth.getGasPrice();
+		return web3.utils.fromWei(gasPrice, 'gwei');
+	} else if (chain === 'stellar') {
+		const server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
+		const feeStats = await server.feeStats();
+		return feeStats.fee_charged.mode;
+	}
+}
+
+module.exports = { calculateFee };
