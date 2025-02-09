0414e282abcf733a42ea4a98cf5d963d39797fad
emmanuelist <emmanuel.paul75@yahoo.com>
2025-02-09 01:16:39 +0100

    Refactor sendPayment function to use frontend signing and update Web3 initialization

--- backend/src/services/ethereumService.js
+++ backend/src/services/ethereumService.js
@@ -1,25 +1,41 @@
 const Web3 = require('web3');
-const web3 = new Web3('https://sepolia.infura.io/v3/YOUR_INFURA_KEY');
 
-// Sepolia USDC Contract (Testnet)
+// Initialize Web3 with Infura
+const web3 = new Web3('https://sepolia.infura.io/v3/YOUR_INFURA_PROJECT_ID');
+
+// USDC Contract Details
 const USDC_ADDRESS = '0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238';
-const ERC20_ABI = [{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"type":"function"}];
+const ERC20_ABI = [
+    {
+        constant: false,
+        inputs: [
+            { name: '_to', type: 'address' },
+            { name: '_value', type: 'uint256' },
+        ],
+        name: 'transfer',
+        outputs: [{ name: '', type: 'bool' }],
+        type: 'function',
+    },
+];
 
-async function sendPayment(senderPrivateKey, recipientAddress, amount) {
-  const sender = web3.eth.accounts.privateKeyToAccount(senderPrivateKey);
-  const contract = new web3.eth.Contract(ERC20_ABI, USDC_ADDRESS);
-  
-  const txData = contract.methods.transfer(recipientAddress, web3.utils.toWei(amount, 'mwei')).encodeABI(); // USDC has 6 decimals
-  
-  const tx = {
-    from: sender.address,
-    to: USDC_ADDRESS,
-    data: txData,
-    gas: 200000,
-    gasPrice: await web3.eth.getGasPrice(),
-    nonce: await web3.eth.getTransactionCount(sender.address),
-  };
+async function sendPayment(senderAddress, recipientAddress, amount) {
+    const contract = new web3.eth.Contract(ERC20_ABI, USDC_ADDRESS);
 
-  const signedTx = await web3.eth.accounts.signTransaction(tx, senderPrivateKey);
-  return web3.eth.sendSignedTransaction(signedTx.rawTransaction).then(res => res.transactionHash);
+    const txData = contract.methods
+        .transfer(recipientAddress, web3.utils.toWei(amount, 'mwei'))
+        .encodeABI();
+
+    const tx = {
+        from: senderAddress,
+        to: USDC_ADDRESS,
+        data: txData,
+        gas: 200000,
+        gasPrice: await web3.eth.getGasPrice(),
+        nonce: await web3.eth.getTransactionCount(senderAddress),
+    };
+
+    // The frontend will sign the transaction using MetaMask
+    return tx;
 }
+
+module.exports = { sendPayment };
